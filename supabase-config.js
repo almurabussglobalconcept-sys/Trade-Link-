6bb335255d8c068a3841ad3dd16aa8e0bf2b175e
almurabussglobalconcept-sys <almurabussglobalconcept@gmail.com>
2026-09-06 12:01:27 +0100

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ygiwekipneoztqvdwahs.supabase.co";
+
+const SUPABASE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";sb_publishable_awSHJPcT5gXUAxdVQiJdqQ_nWPoXJwZ
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
